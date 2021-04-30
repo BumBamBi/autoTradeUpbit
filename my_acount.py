ab4c40631fb6053fa8074091eddcce40683e970f
KWANGWOO LEE <dlrhkddn0325@gmail.com>
2021-04-30 19:03:07 +0900

    input API keys

--- my_acount.py
+++ my_acount.py
@@ -1,7 +1,7 @@
 import pyupbit
 
-access = "---"          # 본인 값으로 변경
-secret = "---"          # 본인 값으로 변경
+access = "2EN4KQ4xxbH4wHmYt5pHLVCP2dZmsgxzwg5qxd1W"          # 본인 값으로 변경
+secret = "GDIl2jiQxd6bYaPBo37BNZGGi6vBYr9MLTxpm7f4"          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
 print(upbit.get_balance("KRW-BTC"))     # KRW-XRP 조회
